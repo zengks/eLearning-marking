71906464800e8ad7ee4c66e357b679be96f3aa99
steven <zengks@outlook.com>
2023-06-08 15:41:21 -0700

    added profile page to view user information

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,7 +1,28 @@
-import React from "react"
+import { useSelector } from "react-redux"
+import { Container, Card } from "react-bootstrap"
 
 const ProfileScreen = () => {
-  return <div>ProfileScreen</div>
+  const { userInfo } = useSelector((state) => state.auth)
+
+  return (
+    <Container>
+      <h1>Profile</h1>
+      <Card style={{ width: "500px", margin: "0 auto" }}>
+        <Card.Body>
+          <Card.Title style={{ fontSize: "2rem" }}>
+            <strong>{`${userInfo.firstName} ${userInfo.lastName}`}</strong>
+          </Card.Title>
+          <Card.Text>
+            <strong>Email</strong>: {userInfo.email}
+          </Card.Text>
+          <Card.Text>
+            <strong>User Role</strong>:
+            {userInfo.isAdmin ? " Instructor" : " Student"}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
 }
 
 export default ProfileScreen
